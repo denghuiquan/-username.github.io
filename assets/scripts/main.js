bf5573457a91aa53e041235b2cc8aa62615df535
denghuiquan <980352129@qq.com>
2022-06-29 20:49:33 +0800

    Update 2022-06-29T12:49:21.755Z

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// TODO: site logics
+$($ => {
+  const $body = $('html, body');
+  $('#scroll_top').on('click', () => {
+    $body.animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+});
